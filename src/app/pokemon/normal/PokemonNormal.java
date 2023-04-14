596bf659446378d32d45117a62cc3651eef21463
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-14 13:43:53 +0100

    modified: added constructor

--- src/app/pokemon/normal/PokemonNormal.java
+++ src/app/pokemon/normal/PokemonNormal.java
@@ -8,7 +8,14 @@ public class PokemonNormal {
     protected double altura;
     protected double peso;
     protected String descripcion;
-
+    
+    public PokemonNormal(String nombre, String tipo, double altura, double peso, String descripcion) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.peso = peso;
+        this.descripcion = descripcion;
+    }
    
 }
     
