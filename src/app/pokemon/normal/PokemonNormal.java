c14ef1a560047605bd8aee2149e8239be56f565c
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-14 13:03:30 +0100

    feat: added atrivuteds

---
+++ src/app/pokemon/normal/PokemonNormal.java
@@ -0,0 +1,15 @@
+package app.pokemon.normal;
+
+
+
+public class PokemonNormal {
+    protected String nombre;
+    protected String tipo;
+    protected double altura;
+    protected double peso;
+    protected String descripcion;
+
+   
+}
+    
+
