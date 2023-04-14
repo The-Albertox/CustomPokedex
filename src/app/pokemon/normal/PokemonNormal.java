1d237c9564a608ae9365aabfabefa8769ad1a854
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-14 13:51:48 +0100

    modified: added getter and setter

--- src/app/pokemon/normal/PokemonNormal.java
+++ src/app/pokemon/normal/PokemonNormal.java
@@ -16,6 +16,42 @@ public class PokemonNormal {
         this.peso = peso;
         this.descripcion = descripcion;
     }
+    public PokemonNormal(String nombre, String tipo) {
+        this(nombre, tipo, 0.0, 0.0, "");
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
    
 }
     
