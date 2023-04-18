a87b02375f0127299118c26ab57ea5df03ecb175
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-18 17:04:46 +0100

    feat: initialy the atrivuted fro the menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+import app.pokemon.lista.ListaInteigente;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    
+    public static void main(String[] args) throws Exception {
+      Scanner teclado = new Scanner(System.in);
+      String nombre = " ";
+      String tipo = " ";
+      int peso;
+      int altura;
+      String descripcion;
+      String localizacion = " ";
+      int numero;
+      String respuesta = " ";
+      ListaInteigente listaAvistados = new ListaInteigente();
+      ListaInteigente listaCapturados = new ListaInteigente();
+      
+
     }
 }
