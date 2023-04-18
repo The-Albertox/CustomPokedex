14c31739d516d0e3dfa8320ba4f284b11a7d1cfd
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-18 10:07:27 +0100

    feat: added contrustor & create list with 2string

--- src/app/pokemon/lista/ListaInteigente.java
+++ src/app/pokemon/lista/ListaInteigente.java
@@ -1,6 +1,29 @@
 package app.pokemon.lista;
 
 public class ListaInteigente {
-    
+    private String[] stringArray;
+    private int totalStrings = 0;
+
+  public ListaInteigente() {
+    this.stringArray = new String[1];
+  }
+
+  public void add(String texto) {
+    String[] newStringArray = new String[this.totalStrings + 1];
+
+    for (int i = 0; i < this.stringArray.length; i++) {
+      newStringArray[i] = this.stringArray[i];
+    }
+
+    newStringArray[this.totalStrings] = texto;
+    this.stringArray = newStringArray;
+    this.totalStrings++;
+  }
+
+  public void display() {
+    for (String string : this.stringArray) {
+      System.out.println("-> " + string);
+    }
+  }
     
 }
