e3fcae6b7c7b29574a83a9212ce642f8862df513
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-19 10:51:53 +0100

    feat add remove feature

--- src/app/pokemon/lista/ListaInteigente.java
+++ src/app/pokemon/lista/ListaInteigente.java
@@ -1,14 +1,16 @@
 package app.pokemon.lista;
 
+import app.pokemon.normal.PokemonNormal;
+
 public class ListaInteigente {
-    private String[] stringArray;
-    private int totalStrings = 0;
+    protected String[] stringArray;
+    protected int totalStrings = 0;
 
   public ListaInteigente() {
     this.stringArray = new String[1];
   }
 
-  public void add(String texto) {
+  public void aniadir(String texto) {
     String[] newStringArray = new String[this.totalStrings + 1];
 
     for (int i = 0; i < this.stringArray.length; i++) {
@@ -19,11 +21,45 @@ public class ListaInteigente {
     this.stringArray = newStringArray;
     this.totalStrings++;
   }
+  public void eliminar(String text) {
 
-  public void display() {
+    int totalElements = 0;
+    for (int i = 0; i < this.stringArray.length; i++) {
+      if (this.stringArray[i].equals(text)){
+        this.stringArray[i] = null;
+      } else {
+        totalElements++;
+      }
+    }
+
+    String[] newStringArray = new String[totalElements];
+    int newArrayIndex = 0;
+
+    for (int i = 0; i < this.stringArray.length; i++) {
+      if (this.stringArray[i] != null){
+        newStringArray[newArrayIndex] = this.stringArray[i];
+        newArrayIndex++;
+      }
+    }
+
+    this.stringArray = newStringArray;
+    this.totalStrings = totalElements;
+  }
+
+  public void mostrar() {
     for (String string : this.stringArray) {
       System.out.println("-> " + string);
     }
   }
+
+  public void aniadir(PokemonNormal pokemonCapturado) {
+  }
+
+  public void eliminar(PokemonNormal pokemonCapturado) {
+  }
+
+public PokemonNormal retornar(int numero) {
+    return null;
+}
     
 }
