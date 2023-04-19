b67e2d39c64578f5c7de3cd5529ab80360a3fa86
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-19 10:51:00 +0100

    feat menu created know problems are aware

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
+import app.pokemon.legendario.PokemonLegendario;
 import app.pokemon.lista.ListaInteigente;
+import app.pokemon.normal.PokemonNormal;
 
 public class Main {
     
@@ -28,6 +30,72 @@ public class Main {
         System.out.println("f. cerrar la pokedex");
         opcion = teclado.nextLine();
         
+        switch (opcion) {
+          case "a":
+          System.out.println("por favo, inserte en nombre y el tipo del pokemon visto");
+          System.out.println("nombre: "); nombre = teclado.nextLine();
+          System.out.println("tipo: "); tipo = teclado.nextLine();
+
+          PokemonNormal pokemonN = new PokemonNormal(nombre, tipo);
+          listaAvistados.aniadir(pokemonN);
+
+          System.out.println("\n" + "pokemon aniadido a avistados" + "\n");
+            
+            break;
+
+          case"b":
+
+            System.out.println("por favor ponga el nombre y el tipo del pokemon i la localizacion del legendario avistado");
+            System.out.println("nombre: "); nombre = teclado.nextLine();
+            System.out.println("tipo: "); tipo = teclado.nextLine();
+            System.out.println("localizacion: "); localizacion = teclado.nextLine();
+
+            PokemonLegendario pokemonLeg = new PokemonLegendario(nombre, tipo, numero, localizacion);
+            System.out.println("\n" + "pokemon legendaio avistado aniadido" + "\n");
+            break;
+          
+          case"c":
+          System.out.println("Por favor inserte el número del pokemon que ha capturado");
+
+          listaAvistados.mostrar(); 
+          numero = teclado.nextInt();
+          PokemonNormal pokemonCapturado = listaAvistados.retornar(numero);
+
+          System.out.println("Pokemon añadido a capturados"); 
+        listaCapturados.aniadir(pokemonCapturado);
+        listaAvistados.eliminar(pokemonCapturado);
+
+       System.out.println("Por favor inserte los siguientes parámetros para finalizar la cap");
+       System.out.println("Peso: ");
+
+       peso= teclado.nextInt(); 
+       pokemonCapturado.setPeso(peso);
+
+      System.out.println( "Altura: "); 
+      altura = teclado.nextInt();
+
+      pokemonCapturado.setAltura(altura);
+      teclado.nextLine();
+      
+      System.out.println( "Descripcion : "); 
+      descripcion = teclado.nextLine();
+      pokemonCapturado.setDescripcion(descripcion);
+
+        break;
+          case "d":
+          listaAvistados.mostrar();
+          break;
+          case "e":
+          listaCapturados.mostrar();
+          break;
+          case "f":
+          salida = true;
+          break;
+
+
+          default:
+            break;
+        }
       }
 
     }
