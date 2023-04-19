d32b188b8b5bdc2a91e502eae2f74d19d78de40e
the-albertox <usr3562@salesianos-lacuesta.net>
2023-04-19 09:30:05 +0100

    feat initaly the menu

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,19 @@ public class Main {
       String respuesta = " ";
       ListaInteigente listaAvistados = new ListaInteigente();
       ListaInteigente listaCapturados = new ListaInteigente();
-      
+      boolean salida = false;
+      String opcion = " ";
+      while (salida ==  false) {
+        System.out.println(" muy buenas ");
+        System.out.println("a. aniadir un pokemon avistado");
+        System.out.println("b. aniadir avistamiento legendario");
+        System.out.println("c. pasara a capturado");
+        System.out.println("d. mostrar la lista de avistados");
+        System.out.println("e. mostrar la lista de capturados");
+        System.out.println("f. cerrar la pokedex");
+        opcion = teclado.nextLine();
+        
+      }
 
     }
 }
